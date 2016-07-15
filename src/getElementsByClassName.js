a48f6d6929cd977e621299069da27504d90790a2
Derek Liu <liu.derekt@gmail.com>
2016-07-15 16:12:57 -0700

    Added recursive method to getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,6 +6,25 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className) {
   var elements = [];
+
+  var grabElements = function (currentNode) {
+
+    // If classList in current node contains the className...
+    if (_.contains(currentNode.classList, className)) {
+
+      // ...store node into Elements list
+      elements.push(currentNode);
+    }
+
+    // Check every node's child nodes
+    for (var key in currentNode.childNodes) {
+      // by using the recursive method
+      grabElements(currentNode.childNodes[key]);
+    }
+  };
   
+  // use the current document body
+  grabElements(document.body);
+
   return elements;
 };
